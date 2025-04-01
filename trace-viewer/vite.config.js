d492a1b17e1fe4cba6cba5e40e124b4cc26e8843
Percy Liang <percyliang@gmail.com>
2025-04-01 00:45:35 -0700

    fix

--- trace-viewer/vite.config.js
+++ trace-viewer/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://stanford-cs336.github.io/spring2025-lectures/',
   plugins: [react()],
 })
